8c67d9d7be4807698791abea947688cb785de215
AdarshMishra26 <adarshmishra2608@gmail.com>
2024-08-09 22:06:35 +0530

    otp function done successfully

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,14 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = {
     ensureAuthenticated: (req, res, next) => {
-        const token = req.headers.authorization.split(' ')[1];
+        const authHeader = req.headers.authorization;
+        if (!authHeader) return res.status(401).json({ error: 'No token provided' });
 
-        jwt.verify(token, process.env.SESSION_SECRET, (err, decoded) => {
+        const token = authHeader.split(' ')[1];
+        if (!token) return res.status(401).json({ error: 'Token format is invalid' });
+
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
             if (err || !decoded.otpVerified) {
                 return res.status(401).json({ error: 'Unauthorized access' });
             }
